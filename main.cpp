efb9ad90c1bef4de19e0116553d198d750683175
Tucker Neikirk <70465212+tuckerN21@users.noreply.github.com>
2025-10-06 13:23:40 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,10 +7,10 @@ Purpose: Run the Pokémon Trainer program. Handles wild encounters, item
 
 #include "Trainer.h"
 
-//wild Pokemon (read in from a file) list size
+// Wild Pokemon (read in from a file) list size
 const int WILD_POKEMON_LIST_SIZE = 25;
 
-//error checking for input of y or n
+// Error checking for input of y or n
 char charInputCheck()
 {
 	char input;
@@ -24,7 +24,7 @@ char charInputCheck()
 }
 
 
-//given - generate both a wild Pokemon and an item
+// Generate both a wild Pokemon and an item
 void generateEncounter(string pokeList[], string &encounteredPokemon, string &pickedUpItem)
 {
 	srand(time(NULL));
@@ -82,7 +82,6 @@ int main()
 	string currPokemon = "";
 	string pokemonList[WILD_POKEMON_LIST_SIZE];
 
-	//uncomment each of the tests as you finish writing the code in Trainer.cpp
 	cout << "Testing readFile()...\n" << endl;
 	readFile(pokemonList, "PokemonList.txt");
 
@@ -128,13 +127,13 @@ int main()
 	
 	while(stillPlaying)
 	{
-		//generate an encounter to get the random item and the wild Pokemon
+		// Generate an encounter to get the random item and the wild Pokemon
 		generateEncounter(pokemonList, wildPokemon, item);
 		
-		//fulfill item encounter
-        cout << "You found a(n) " << item << ". Do you want to keep it? (y/n)" << endl; // ask user if they want to keep the item
+		// Fulfill item encounter
+        cout << "You found a(n) " << item << ". Do you want to keep it? (y/n)" << endl; // Ask user if they want to keep the item
         
-        // if-else statements to check index of the item that the trainer has found
+        // If-else statements to check index of the item that the trainer has found
 		if (item == "Poke Ball") {
             itemIndex = Trainer::POKE_BALL;
         } 
@@ -147,26 +146,26 @@ int main()
         
         input = charInputCheck();
         
-        if (input == 'y') // if user says yes
+        if (input == 'y') // If user says yes
         {
-            if (player.addItem(itemIndex) == true) // add item if they have room
+            if (player.addItem(itemIndex) == true) // Add item if they have room
             {
                 cout << "Successfully added " << item << " to your inventory!" << endl;
             }
-            else // otherwise, let them know they don't have room
+            else // Otherwise, let them know they don't have room
             {
                 cout << "You don't have enough room for that type of item." << endl;
             }
         }
-        else if (input == 'n') // if user says no
+        else if (input == 'n') // If user says no
         {
             cout << "You put the " << item << " back." << endl;
         }
 
-		//print stats
+		// Print stats
 		player.printStats();
 		
-		//generate steps
+		// Generate steps
 		int numSteps = rand() % 10 + 1;
 		for (int i = 1; i <= numSteps; i++)
 		{ 
@@ -176,23 +175,23 @@ int main()
 				cout << "You've taken " << i << " steps." << endl;
 		}
 
-		//fulfill Pokemon encounter
-		cout << "You've encountered a(n) " << wildPokemon << "!" << endl; // user encounters wild pokemon
-		cout << "Do you want to fight it?" << endl; // ask user if they want to fight the wild pokemon
+		// Fulfill Pokemon encounter
+		cout << "You've encountered a(n) " << wildPokemon << "!" << endl; //  User encounters wild pokemon
+		cout << "Do you want to fight it?" << endl; // Ask user if they want to fight the wild pokemon
 		
-		input = charInputCheck(); // error check user's input
+		input = charInputCheck(); // Error check user's input
 		
-		if (input == 'y') // if user wants to fight
+		if (input == 'y') // If user wants to fight
         {
             
-            // display the pokemon in user's trainer class and have them pick one
+            // Display the pokemon in user's trainer class and have them pick one
             cout << "Which Pokemon do you want to use?" << endl;
             for (int i = 0; i < player.getNumPokemon(); i++)
             {
                 cout << i + 1 << ") " << player.getPokemon(i) << endl;
             }
             
-            do { // do-while loop for error checking user input
+            do { // Do-while loop for error checking user input
                 cout << ">> ";
                 cin >> choice;
                 if (choice < 1 || choice > player.getNumPokemon())
@@ -201,57 +200,57 @@ int main()
                 }
             } while (choice < 1 || choice > player.getNumPokemon());
             
-            int win = rand() % 2; // generate random number for results of battle
+            int win = rand() % 2; // Generate random number for results of battle
             
-            if (win == 0) // user loses
+            if (win == 0) // User loses
             {
-                // user's pokemon faints and the wild one runs away
+                // User's pokemon faints and the wild one runs away
                 cout << "Oh no! " << player.getPokemon(choice - 1) << " fainted and the " << wildPokemon << " escaped! "; 
                 
-                // check if user has revives
-                if (player.getItemAmount(Trainer::REVIVE) == 0) // if user doesn't have revives
+                // Check if user has revives
+                if (player.getItemAmount(Trainer::REVIVE) == 0) // If user doesn't have revives
                 {
-                    // let user know they're out of revives and remove chosen pokemon from inventory
+                    // Let user know they're out of revives and remove chosen pokemon from inventory
                     cout << endl << "Looks like you're out of revives. " << player.getPokemon(choice - 1) << " was transferred to Professor Oak." << endl;
                     player.removePokemon(choice - 1);
                 }
-                else if (player.getItemAmount(Trainer::REVIVE) != 0) // if user does have revives
+                else if (player.getItemAmount(Trainer::REVIVE) != 0) // If user does have revives
                 {
-                    // ask user if they want to use a revive
+                    // Ask user if they want to use a revive
                     cout << "Use a Revive? (y/n)" << endl; 
                     input = charInputCheck();
                     
-                    // if-else statements for user input
+                    // If-else statements for user input
                     if (input == 'y') // if user says yes
                     {
-                        // use function to update inventory and display that pokemon was healed
+                        // Use function to update inventory and display that pokemon was healed
                         player.useItem(Trainer::REVIVE); 
                         cout << player.getPokemon(choice - 1) << " was healed. " << player.getItemAmount(Trainer::REVIVE) << " revive(s) remaining." << endl; 
                     }
-                    else if (input == 'n') // if user says no
+                    else if (input == 'n') // If user says no
                     {
-                        // remove pokemon from inventory using function
+                        // Remove pokemon from inventory using function
                         cout << player.getPokemon(choice - 1) << " was transferred to Professor Oak." << endl;
                         player.removePokemon(choice - 1);
                     }
                 }
                 
             }
-            else if (win == 1) // user wins
+            else if (win == 1) // User wins
             {
-                // give user the option of catching the wild pokemon
+                // Give user the option of catching the wild pokemon
                 cout << "You and " << player.getPokemon(choice - 1) << " won! Do you want to catch the " << wildPokemon << "?" << endl;
                 input = charInputCheck();
                 
-                if (input == 'y') // if user says yes
+                if (input == 'y') // If user says yes
                 {
-                    // check if user's inventory is maxed out
-                    if (player.hasPokemonSpace() == true) // if user has inventory space
+                    // Check if user's inventory is maxed out
+                    if (player.hasPokemonSpace() == true) // If user has inventory space
                     {
-                        // make sure user has poke balls or ultra balls on hand
-                        if (player.getItemAmount(Trainer::POKE_BALL) > 0 || player.getItemAmount(Trainer::ULTRA_BALL) > 0) // if user has poke balls or ultra balls
+                        // Make sure user has poke balls or ultra balls on hand
+                        if (player.getItemAmount(Trainer::POKE_BALL) > 0 || player.getItemAmount(Trainer::ULTRA_BALL) > 0) // If user has poke balls or ultra balls
                         {
-                            // let user choose what they want to use
+                            // Let user choose what they want to use
                             cout << "What ball do you want to use?" << endl;
                             if (player.getItemAmount(Trainer::POKE_BALL) > 0)
                             {
@@ -262,7 +261,7 @@ int main()
                                 cout << "2) Ultra Ball (" << player.getItemAmount(Trainer::ULTRA_BALL) << " remaining)" << endl;
                             }
                             
-                            do { // do-while loop to error check user input
+                            do { // Do-while loop to error check user input
                                 cout << ">> ";
                                 cin >> choice;
                                 if ((choice == 1 && player.getItemAmount(Trainer::POKE_BALL) == 0) || (choice == 2 && player.getItemAmount(Trainer::ULTRA_BALL) == 0)) 
@@ -277,62 +276,59 @@ int main()
                                 }
                             } while (choice != 1 && choice != 2);
                             
-                            // user has 100% chance of catching the wild pokemon
+                            // User has 100% chance of catching the wild pokemon
                             cout << "You caught " << wildPokemon << "!" << endl;
-                            player.useItem(choice - 1); // update item inventory using function
-                            player.addPokemon(wildPokemon); // update pokemon inventory using function
+                            player.useItem(choice - 1); // Update item inventory using function
+                            player.addPokemon(wildPokemon); // Update pokemon inventory using function
 
                         } 
-                        else // if user doesn't have poke balls or ultra balls
+                        else // If user doesn't have poke balls or ultra balls
                         {
-                            cout << "You don't have any balls to catch the wild Pokémon." << endl; // display a message
+                            cout << "You don't have any balls to catch the wild Pokémon." << endl; // Display a message
                         }
                     }
-                    else if (player.hasPokemonSpace() == false) // if user does not have inventory space
+                    else if (player.hasPokemonSpace() == false) // If user does not have inventory space
                     {
-                        cout << "Looks like you don't have enough space in your inventory." << endl; // display a message letting user know they have no space
+                        cout << "Looks like you don't have enough space in your inventory." << endl; // Display a message letting user know they have no space
                     }
                 }
-                else if (input == 'n') // otherwise, leave wild pokemon and let user continue
+                else if (input == 'n') // Otherwise, leave wild pokemon and let user continue
                 {
                     cout << "You leave the wild " << wildPokemon << " and continue on your way." << endl;
                 }
             }
         }
-        else if (input == 'n') // if user does not want to fight
+        else if (input == 'n') // If user does not want to fight
         {
-            cout << "You chose to run away." << endl; // have user run away
+            cout << "You chose to run away." << endl; // Have user run away
         }
 
-		//check if user can still play, and then if they want to still play
-		// if the user has no pokemon, they can't keep playing
-		if (player.getNumPokemon() == 0) // if user has no pokemon left
+		// Check if user can still play, and then if they want to still play
+		// If the user has no pokemon, they can't keep playing
+		if (player.getNumPokemon() == 0) // If user has no pokemon left
 		{
-		    // display message and set stillPlaying to false
+		    // Display message and set stillPlaying to false
 		    cout << "Looks like you have no Pokémon remaining." << endl;
 		    stillPlaying = false;
 		}
 		
-		//else, see if the user wants to keep playing
-		else // if user has pokemon left
+		// Else, see if the user wants to keep playing
+		else // If user has pokemon left
 		{
-			//note that these 4 lines will likely be inside of an if/else block that is detailed 
-			//at the end of "2. Description" in the homework
-			cout << "Do you want to keep playing?" << endl; // ask user if they want to keep playing
+			cout << "Do you want to keep playing?" << endl; // Ask user if they want to keep playing
 			input = charInputCheck();
 
-			if (input == 'n') // if user says no
-				stillPlaying = false; // set stillPlaying to false
+			if (input == 'n') // If user says no
+				stillPlaying = false; // Set stillPlaying to false
 		}
 		
 	}
-	//create a unique filename with number of seconds passed since Jan 1, 1970 so multiple
-	//runs of the program will save to different files
+	// Create a unique filename with number of seconds passed since Jan 1, 1970 so multiple runs of the program will save to different files
 	string outputFilename = "player-";
 	outputFilename.append(std::to_string(time(0)));
 	outputFilename.append(".txt");
 
-	//save to file
+	//Save to file
 	player.writeFile(outputFilename);
 	
 	cout << "Your player information has been saved to " << outputFilename << ". Until next time!\n";
